d4b9059708dddf9f989915f0d6e2ebb21fa9e0bb
Erick CM <e.cardenasmendez@gmail.com>
2013-03-19 10:52:11 -0400

    info section within options

--- client/js/options.js
+++ client/js/options.js
@@ -6,6 +6,14 @@
 
 
   Template.options.events({
+    'click .question': function(e) {
+      e.preventDefault();
+      var newOffset = $(e.currentTarget).offset().top - $('#options h2').offset().top;
+      $(e.currentTarget).parent().next('.answer').toggle(400, function () {
+        $('html, body').animate({scrollTop: newOffset}, 400);
+      });
+    },
+
     'click #removeAll': function() {
       Session.set("showRemoveAllWarning", true);
     }
